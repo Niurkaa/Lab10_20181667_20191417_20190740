1a79e19cd7b772f6e19eab83ba8dbddf4ba28c45
Niurka Carrión <niurka.carrion@pucp.edu.pe>
2022-06-27 19:10:53 -0500

    Initial commit

---
+++ src/main/java/com/example/lab10_20181667_20191417_20190740/HelloApplication.java
@@ -0,0 +1,9 @@
+package com.example.lab10_20181667_20191417_20190740;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
