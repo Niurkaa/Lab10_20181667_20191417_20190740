3dcb99520ceaf65e8620ca5cff17f79eee537f68
Loammi Jezreel Ugaz Manayay <a20191417@pucp.edu.pe>
2022-06-27 20:05:22 -0500

    Conexión .jar con SQL

--- src/main/java/com/example/lab10_20181667_20191417_20190740/HelloApplication.java
+++
@@ -1,9 +0,0 @@
-package com.example.lab10_20181667_20191417_20190740;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/api")
-public class HelloApplication extends Application {
-
-}
